3d5544801d3fadf4cf3f7c8d0e84a46966440d4c
caku <camkurup@hotmail.com>
2022-03-10 14:22:26 +0100

    * PersonProject.csproj: * PersonModel.cs: * SessionExtensions.cs: * HomeController.cs: * PersonProject.csproj.AssemblyReference.cache:
    * Index.cshtml: A lot of changes have been done at this moment I see
      my form in the browser and I get a Json respons but the sessions
      does not work and there for my Json shows "null"

--- PersonProject/Controllers/HomeController.cs
+++ PersonProject/Controllers/HomeController.cs
@@ -1,40 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PersonProject.Models;
 using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PersonProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private ISession _session => _httpContextAccessor.HttpContext.Session;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHttpContextAccessor httpContextAccessor)
         {
             _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public IActionResult Index()
         {
-            HttpContext.Session.SetString("Test", "Ben Rules!");
-
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewBag.Message = HttpContext.Session.GetString("Test");
-
             return View();
         }
 
         public IActionResult Privacy()
         {
+
             return View();
         }
 
@@ -42,6 +35,26 @@ namespace PersonProject.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        
+        public IEnumerable<PersonModel> AddPerson(string firstName, string lastname)
+        {
+            return Enumerable.Range(1, 1).Select(index => new PersonModel(firstName, lastname)
+            {
+                Name = firstName,
+                Lastname = lastname
+            }).ToArray();
+        }
+        
+
+        public void AddSession()
+        {
+            var myComplexObject = new PersonModel();
+            HttpContext.Session.SetObjectAsJson("Test", myComplexObject);
+
+            HttpContext.Session.GetObjectFromJson<PersonModel>("Test");
+
         }
     }
 }
---
+++ PersonProject/Models/PersonModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace PersonProject.Models
+{
+    public class PersonModel
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+
+        public PersonModel()
+        {
+        }
+
+        public PersonModel(string name, string lastname)
+        {
+            Name = name;
+            Lastname = lastname;
+        }
+    }
+}
