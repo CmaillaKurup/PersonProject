b23a3fa506ca10be1a969fae5222ce76edd8ed8c
CmaillaKurup <cami670v@zbc.dk>
2022-03-10 10:27:21 +0100

    * PersonProject.sln: * Program.cs: * Startup.cs: * appsettings.json: * site.js: * favicon.ico: * PersonProject.csproj: * site.css: * Index.cshtml: * _ViewStart.cshtml: * ErrorViewModel.cs: * Privacy.cshtml: * Error.cshtml: * _ViewImports.cshtml: * _Layout.cshtml: * appsettings.Development.json: * LICENSE: * HomeController.cs: * LICENSE.txt: * launchSettings.json: * jquery.js: * jquery.min.js: * jquery.min.map: * LICENSE.md: * bootstrap.js: * bootstrap.css: * _ValidationScriptsPartial.cshtml: * bootstrap.min.js: * bootstrap.js.map: * bootstrap.css.map: * bootstrap.min.css: * bootstrap-grid.css: * bootstrap.bundle.js: * bootstrap.min.js.map: * bootstrap-reboot.css: * bootstrap.min.css.map: * jquery.validate.js: * LICENSE.txt: * bootstrap-grid.min.css: * bootstrap.bundle.min.js: * bootstrap.bundle.js.map: * bootstrap-grid.css.map: * bootstrap-reboot.min.css: * bootstrap-reboot.css.map: * additional-methods.js: * bootstrap.bundle.min.js.map: * jquery.validate.min.js: * bootstrap-grid.min.css.map: * bootstrap-reboot.min.css.map: * additional-methods.min.js: * PersonProject.csproj.AssemblyReference.cache: * jquery.validate.unobtrusive.js: * jquery.validate.unobtrusive.min.js:
    * .gitignore: general commit

---
+++ PersonProject/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PersonProject.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace PersonProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("Test", "Ben Rules!");
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = HttpContext.Session.GetString("Test");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
